24244cc827c21e7aa34e2054e2b5c6c703f1f22b
icis-04 <chidexiwuaku@gmail.com>
2023-02-08 02:40:29 +0100

    added get method

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,7 +25,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route('/unauthorized', strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def stats() -> str:
     """ Get /api/v1/unauthorized
     Return:
