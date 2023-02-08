2ff5d66ea92ea348d1a2d2e288785bf2a7906dfe
icis-04 <chidexiwuaku@gmail.com>
2023-02-08 03:14:57 +0100

    changed name

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def stats() -> str:
+def authorized() -> str:
     """ Get /api/v1/unauthorized
     Return:
         - the status of the API
