9377a61b80bf9fbaa5f47ed5a2e15fd49ef9853a
icis-04 <chidexiwuaku@gmail.com>
2023-02-11 00:12:12 +0100

    startswith implemented

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,10 +13,8 @@ class Auth:
         if path == None or excluded_paths == None or excluded_paths == []:
             return True
         for i in excluded_paths:
-            if i == path or i == path + '/':
+            if i.startswith(path):
                 return False
-            elif i != path or i != path + '/':
-                return True
         return False
     def authorization_header(self, request=None) -> str:
         """ function that handles the authorization header """
