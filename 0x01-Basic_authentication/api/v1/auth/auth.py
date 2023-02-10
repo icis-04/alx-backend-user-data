7a32ae3395e232ef0c01a254322326de43cf041d
icis-04 <chidexiwuaku@gmail.com>
2023-02-10 19:57:46 +0100

    modified require_auth for loop

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,6 +15,8 @@ class Auth:
         for i in excluded_paths:
             if i == path or i == path + '/':
                 return False
+            elif  i != path ior i != path + '/':
+                retirn True
         return False
     def authorization_header(self, request=None) -> str:
         """ function that handles the authorization header """
