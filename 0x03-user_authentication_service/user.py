146b57fedb6a4fd13032f0f8de1c8e13a208ad86
icis-04 <chidexiwuaku@gmail.com>
2023-02-15 16:48:44 +0000

    sql alchemy is a go

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" script to define the database schema for this project """
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+class User(Base):
+    """ Base class for the User model """
+    __tablename__ = 'users'
+
+    id: int = Column(Integer, primary_key=True)
+    email: str = Column(String(250), nullable=False)
+    hashed_password: str = Column(String(250), nullable=False)
+    session_id: str = Column(String(250), nullable=False)
+    reset_token: str = Column(String(250), nullable=False)
+
